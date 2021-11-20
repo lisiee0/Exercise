c6d3d41a35d2f6213bfd5b3e2d6990903b8e7b6d
Hojun_Yoo <lisiee0@naver.com>
2021-11-20 23:08:42 +0900

    baekjoon ex

---
+++ src/baekjoon/Ex01.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  ) ");
+		System.out.println(" \\(__)|");
+
+	}
+}
---
+++ src/baekjoon/Ex02.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int num1= sc.nextInt();
+		int num2= sc.nextInt();
+		
+		if(num1>num2) {
+			System.out.println(">");
+		}
+		else if(num1<num2) {
+			System.out.println("<");
+		}
+		else {
+			System.out.println("==");
+		}
+
+		sc.close();
+	}
+}
