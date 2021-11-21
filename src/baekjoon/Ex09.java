1a86652d19c18b1c8daa3d33c660ff8be07b3cbe
Hojun_Yoo <lisiee0@naver.com>
2021-11-21 22:46:50 +0900

    ex07, 08, 09

---
+++ src/baekjoon/Ex09.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int num= sc.nextInt();
+		int sum= 0;
+		
+		for(int i= 1; i<=num; i++) {
+			sum= sum+i;
+		}
+		System.out.println(sum);
+		
+		sc.close();
+	}
+}
