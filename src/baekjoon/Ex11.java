7c68af3d5071dbfad95ea00402f936c9a9edfbe4
Hojun_Yoo <lisiee0@naver.com>
2021-11-21 22:54:10 +0900

    10 11

---
+++ src/baekjoon/Ex11.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int num= sc.nextInt();
+		
+		for(int i= num; i>0; i--) {
+			System.out.println(i);
+		}
+		
+		sc.close();	
+	}
+}
