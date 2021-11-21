248d5d498ba2fa7ebc25e443ac39bd59edf32a29
Hojun_Yoo <lisiee0@naver.com>
2021-11-21 23:01:22 +0900

    12 13

---
+++ src/baekjoon/Ex13.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int casenum= sc.nextInt();
+		
+		int[] case1= new int[casenum];
+		int[] case2= new int[casenum];
+		
+		for(int i= 0; i<case1.length; i++) {
+			case1[i]= sc.nextInt();
+			case2[i]= sc.nextInt();
+		}
+		for(int i= 0; i<case1.length; i++) {
+			System.out.println("Case #"+(i+1)+": "+case1[i]+" + "+case2[i]+" = "+(case1[i]+case2[i]));
+		}
+		sc.close();
+
+	}
+
+}
