8d94baf986b0b0aeabdd11110b9e8770c0dd338d
Hojun_Yoo <lisiee0@naver.com>
2021-11-27 22:29:12 +0900

    ex24

---
+++ src/baekjoon/Ex24.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Ex24 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int n01= sc.nextInt();
+		int n02= sc.nextInt();
+		int n03= sc.nextInt();
+		
+		int result= n01*n02*n03;
+		
+		int[] count= {0,0,0,0,0,0,0,0,0,0};
+		
+		for(int i= 0; i<count.length; i++) {
+			if(result<10000000) {
+				
+			}
+		}
+		
+		sc.close();
+		
+
+	}
+
+}
