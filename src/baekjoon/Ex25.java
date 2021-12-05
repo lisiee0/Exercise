6a02493bd63ed33fc07b6fb680f1f46a3dc13df6
Hojun_Yoo <lisiee0@naver.com>
2021-12-05 22:25:38 +0900

    ex25

--- src/baekjoon/Ex25.java
+++ src/baekjoon/Ex25.java
@@ -7,26 +7,20 @@ public class Ex25 {
 	public static void main(String[] args) {
 		
 		Scanner sc= new Scanner(System.in);
-		
-		int[] nArray= new int[10];
-		int[] com= new int[10];
+
+		int[] com= new int[42];
 		int count= 0;
 		
-		for(int i= 0; i<nArray.length; i++) {
-			nArray[i]= sc.nextInt();
+		for(int i= 0; i<10; i++) {
+			int num= sc.nextInt();
+			com[num%42]++;
 		}
 		
 		for(int i= 0; i<com.length; i++) {
-			com[i]=nArray[i]%42;
-			if(i==0) {
-				count= 1;
+			if(com[i]!=0) {
+				count++;
 			}
-			else {
-				if(com[i]!=com[i-1]) {
-					count++;
-				}
-			}
-		}
+		}			
 		System.out.println(count);
 		sc.close();
 	}
