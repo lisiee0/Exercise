a06117befaeab6a9722c33c0beb863d3f99fd05b
Hojun_Yoo <lisiee0@naver.com>
2021-12-12 12:10:26 +0900

    ex29

--- src/baekjoon/Ex29.java
+++ src/baekjoon/Ex29.java
@@ -7,25 +7,26 @@ public class Ex29 {
 	public static void main(String[] args) {
 		
 		int result= 0;
-		int index= 1;
+		int score= 1;
 		Scanner sc= new Scanner(System.in);
 		
-        int caseNum= sc.nextInt();
+        String[] strArray= new String[sc.nextInt()];
         
-        for (int i= 0; i<caseNum; i++) {
-            String c= sc.nextLine();
+        for(int i= 0; i<strArray.length; i++) {
+        	strArray[i]= sc.nextLine();
         }
-
-        for (int j= 0; j<c.length(); j++) {
-            if (c.equals("O")) {
-                result+= index;
-                index++;
-            } 
-            else if (c.equals("X")) {
-                index= 1;
-            } 
+        
+        for (int i= 0; i<strArray.length; i++) {
+            for(int j=0; j<strArray[i].length(); j++) {
+            	if(strArray[i].charAt(j)=='O') {
+            		result+= score;
+            		score++;
+            	}
+            	else if(strArray[i].charAt(j)=='X') {
+            		score= 1;
+            	}	
+            }   
         }
-    }	
-        System.out.println(result);
         sc.close();
+	}
 }
